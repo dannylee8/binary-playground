9c0cb320dba2a431ae218a4774fd07d63c6667a6
Danny Lee <dreamer@panix.com>
2020-06-27 13:36:05 -0400

    update css and add twos element line

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // Try edit message
-let decimalEle = document.getElementById("decimal-num-text");
-let binaryEle = document.getElementById("binary-num-text");
+let decimalEle = document.getElementById("dec");
+let binaryEle = document.getElementById("bin");
+let twosEle = document.getElementById("twos");
+
 let number = 0;
 let increment = 1;
 
 function upClick() {
   number += increment;
   decimalEle.innerHTML = number;
-  binaryEle.innerHTML = number.toString(2);
+  binaryEle.innerHTML = (number.toString(2) >>> 0).toString(2);
+  twosEle.innerHTML = (~number.toString(2)).toString(2);
 }
 
 function downClick() {
   number -= increment;
   decimalEle.innerHTML = number;
-  binaryEle.innerHTML = number.toString(2);
+  binaryEle.innerHTML = (number.toString(2) >>> 0).toString(2);
+  twosEle.innerHTML = (~number.toString(2) >>> 0).toString(2);
 }
 
 function incrementClick(num) {
