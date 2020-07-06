703dbe7edc1f5c990cbc979db263dd7e2c28d9ce
Danny Lee <dreamer@panix.com>
2020-07-06 01:31:50 -0400

    update calculator page.  add decimal and binary values.  decimal arithmetic implemented.

--- calculator-script.js
+++ calculator-script.js
@@ -1,3 +1,44 @@
+v1 = document.getElementById("value1");
+v2 = document.getElementById("value2");
+d1 = document.getElementById("decimal1");
+d2 = document.getElementById("decimal2");
+dSum = document.getElementById("decimal-sum");
+bSum = document.getElementById("binary-sum");
+bd1 = document.getElementById("binary-display1");
+bd2 = document.getElementById("binary-display2");
+
+v1.addEventListener("change", handleV1);
+v2.addEventListener("change", handleV2);
+
+let decimalNumber1 = 0;
+let decimalNumber2 = 0;
+let binaryNumber1 = 0;
+let binaryNumber2 = 0;
+
+function handleV1(e) {
+  decimalNumber1 = calculateDecimal(e.target.value);
+  d1.innerHTML = decimalNumber1;
+
+  binaryNumber1 = e.target.value;
+  bd1.innerHTML = binaryNumber1;
+}
+
+function handleV2(e) {
+  decimalNumber2 = calculateDecimal(e.target.value);
+  d2.innerHTML = decimalNumber2;
+
+  binaryNumber2 = e.target.value;
+  bd2.innerHTML = binaryNumber2;
+}
+
+function calculateDecimal(num) {
+  return parseInt(num, 2);
+}
+
+function calculateSum() {
+  dSum.innerHTML = decimalNumber1 + decimalNumber2;
+}
+
 // https://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
 function setInputFilter(textbox, inputFilter) {
   ["input"].forEach(function (event) {
@@ -24,5 +65,5 @@ setInputFilter(document.getElementById("value1"), function (value) {
 });
 
 setInputFilter(document.getElementById("value2"), function (value) {
-  return /^[01]$/.test(value);
+  return /^[01]*$/.test(value);
 });
